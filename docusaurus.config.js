9b5fb4f3ec6cb626594465192fcdccede8fdc157
gregsadetsky <lepetitg@gmail.com>
2024-07-02 18:22:48 -0400

    docus upgrade + search

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -94,6 +94,38 @@ const config = {
             autoCollapseCategories: false,
           },
       },
+      algolia: {
+        // The application ID provided by Algolia
+        appId: 'BLNP7BXTWD',
+
+        // Public API key: it is safe to commit it
+        apiKey: '2be8ad1ce65efff21b869dfd33e10039',
+
+        indexName: 'letsdisco',
+
+        // Optional: see doc section below
+        contextualSearch: true,
+
+        // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
+        // externalUrlRegex: 'external\\.com|domain\\.com',
+
+        // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
+        // replaceSearchResultPathname: {
+          // from: '/docs/', // or as RegExp: /\/docs\//
+          // to: '/',
+        // },
+
+        // Optional: Algolia search parameters
+        // searchParameters: {},
+
+        // Optional: path for search page that enabled by default (`false` to disable it)
+        searchPagePath: false, // 'search',
+
+        // Optional: whether the insights feature is enabled or not on Docsearch (`false` by default)
+        insights: false,
+
+        //... other Algolia params
+      },
       colorMode: {
         disableSwitch: true,
         respectPrefersColorScheme: true,
@@ -106,19 +138,19 @@ const config = {
           alt: 'disco',
           src: 'img/disco-logo.svg',
         },
-        // items: [
-        //   // {
-        //   //   type: 'docSidebar',
-        //   //   sidebarId: 'tutorialSidebar',
-        //   //   position: 'left',
-        //   //   label: 'Tutorial',
-        //   // },
-        //   {
-        //     href: "https://github.com/letsdiscodev",
-        //     label: "GitHub",
-        //     position: "right",
-        //   },
-        // ],
+        items: [
+          // {
+          //   type: 'docSidebar',
+          //   sidebarId: 'tutorialSidebar',
+          //   position: 'left',
+          //   label: 'Tutorial',
+          // },
+          {
+            label: "Discord",
+            href: "https://discord.gg/7J4vb5uUwU",
+            position: "right",
+          },
+        ],
       },
       footer: {
         style: "dark",
